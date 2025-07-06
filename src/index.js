dfaf642394e47ad6f4e05c07156cb86471971dd5
Raa0404 <piyushrana007.pr@gmail.com>
2025-07-06 20:33:58 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import './index.css';
+
+const App = () => (
+  <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+    <h1>OTS Final App - All Phases Implemented</h1>
+    <p>This is a placeholder. The final interactive app will load here.</p>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
